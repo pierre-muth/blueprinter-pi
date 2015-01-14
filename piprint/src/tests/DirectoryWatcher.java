581d8fd2130e432df19e701329b6cdaeadc0eac1
muth.inc@gmail.com <muth.inc@gmail.com@ac254708-d01e-da66-a74f-f49d8e753fa0>
2015-01-14 18:26:07 +0000

    tests auto-directory watch

--- piprint/src/tests/DirectoryWatcher.java
+++ piprint/src/tests/DirectoryWatcher.java
@@ -1,4 +1,4 @@
-package piprint;
+package tests;
 
 import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
 
@@ -10,33 +10,17 @@ import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 
+public class DirectoryWatcher {
 
-public class PrinterTests {
-
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		WatchService watcher = null;
 		WatchKey key = null;
-
-		System.out.println("BluePrinter-Pi Test");
-		
-		ThermalPrinter printer = new ThermalPrinter();
-//		printer.configPrinterWithDefault();
-		PrinterConfig pc = new PrinterConfig();
-		pc.heatingMaxDot = 11;
-		pc.heatTime = (byte) 60;
-		pc.heatInterval = (byte) 60;
-		
-		pc.printDensity = 14;
-		pc.printBreakTime = 0;
-		
-		printer.configPrinter(pc);
-		
 		try {
 			watcher = FileSystems.getDefault().newWatchService();
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-		Path dir = Paths.get("/home/pi/project/piprint/bin/piprint");
+		Path dir = Paths.get("c:\\pierre");
 		try {
 			key = dir.register(watcher, ENTRY_CREATE);
 		} catch (IOException e) {
@@ -54,24 +38,12 @@ public class PrinterTests {
 					WatchEvent<Path> ev = (WatchEvent<Path>)event;
 					System.out.println(">"+event.kind()+", "+ev.context());
 					
-					DitheredImage image = new DitheredImage(ev.context().toString());
-					printer.printImage(image);
-					
-					int timeSpent = 0;
-					while(printer.isPrinting()) {
-						Thread.sleep(1000);
-						System.out.print(".");
-						timeSpent++;
-					}
-					System.out.println("Printing total Time: "+timeSpent+"s");
-					
 				}
 				akey.reset();
 			} catch (InterruptedException x) {
 				return;
 			}
 		}
-		
 	}
 
 }
